1b5330bfc01a35fcf1fc5ed23cd37df4533eeea1
Luong Dang <luong.dang@gmail.com>
2023-02-20 07:09:55 -0500

    Updated

--- tmp/scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-image_name=luongdang/morningstar-timeoff-management:1.4.3
-docker image build -t $image_name .
-
-docker run -dp 3000:3000 --rm --name timeoff-management $image_name
-
-/root/.npm/_logs/2023-02-17T18_23_51_231Z-debug-0.log
-alias ll="ls -l --color"
